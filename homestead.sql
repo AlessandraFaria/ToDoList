028a43c69e8aec1d4c2ae3fb16a257593a2aa92a
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2017-04-13 15:15:25 -0300

    Add files via upload

---
+++ homestead.sql
@@ -0,0 +1,113 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 13-Abr-2017 às 18:13
+-- Versão do servidor: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `homestead`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lista`
+--
+
+CREATE TABLE `lista` (
+  `id` int(30) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `estado` varchar(30) NOT NULL,
+  `descricao` varchar(140) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `lista`
+--
+
+INSERT INTO `lista` (`id`, `nome`, `estado`, `descricao`) VALUES
+(1, 'estudos', 'incompleto', 'estudar toda a materia de IC para a prova pos fds'),
+(2, 'estudos', 'incompleto', 'estadar toda a materia de poo para a prova pos fds');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `niveis_acessos`
+--
+
+CREATE TABLE `niveis_acessos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `niveis_acessos`
+--
+
+INSERT INTO `niveis_acessos` (`id`, `nome`, `created`, `modified`) VALUES
+(1, 'Administrador', '2016-02-19 00:00:00', NULL),
+(2, 'Colaborador', '2016-02-19 00:00:00', NULL),
+(3, 'Cliente', '2016-02-19 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(220) NOT NULL,
+  `email` varchar(520) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  `situacoe_id` int(11) NOT NULL DEFAULT '0',
+  `niveis_acesso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `niveis_acessos`
+--
+ALTER TABLE `niveis_acessos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `niveis_acessos`
+--
+ALTER TABLE `niveis_acessos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
